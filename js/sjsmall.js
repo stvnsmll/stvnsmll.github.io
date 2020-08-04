dd5575186adcb1d1fcc44c3b5fa8c553914be603
stvnsmll <48965002+stvnsmll@users.noreply.github.com>
2020-08-04 16:00:16 -0400

    Update sjsmall.js

--- js/sjsmall.js
+++ js/sjsmall.js
@@ -115,4 +115,5 @@ function noEmailYet() {
 
 function showCMIpwd() {
 	alert("\nIMPORTANT!!\n\nThe password to view the engineering efficiency tools is:   worksamples\n\nAfter the password entry, click 'Open in ONLYOFFICE'\n");
+	window.open(https://nextcloud.kleinefamilie.tk/s/Jm7ngcPfwRBPeSg, '_blank');
 }
